922eab461e6d395c00d0c0344253c66404fc67ef
r00tyy <82049406@qq.com>
2018-07-07 17:30:30 +0800

    Update create_data.php

--- Lesson-17/create_data.php
+++ Lesson-17/create_data.php
@@ -7,6 +7,8 @@ if (!$con)
   }
 
 /**
+//创建数据库
+//如果还没有 my-db 的数据库， 那么这段注释掉，先创建数据库
 if (mysql_query("CREATE DATABASE my_db",$con))
   {
   echo "Database created";
@@ -18,7 +20,9 @@ else
 **/
 
 
-/**
+///**
+//创建数据表
+//如果 my-db 的数据库没有数据表 Persons， 那么这段注释掉，先创建数据表
 mysql_select_db("my_db", $con);
 $sql = "CREATE TABLE Persons
 (
@@ -26,13 +30,19 @@ FirstName varchar(15),
 LastName varchar(15),
 Age int
 )";
-**/
+//**/
 
 
+//添加数据
 mysql_select_db("my_db", $con);
 $sql = "insert into Persons set Firstname='test', Lastname='abc', Age=18";
-
 mysql_query($sql,$con);
+
+
+//查询数据
+$sql = "select * from Persons";
+$rows = mysql_fetch_rows($sql);
+print_r($rows)
 
 mysql_close($con);
 
